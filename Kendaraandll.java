e4c2c0d19843ef5f1f6a0538b08d06eda377d20f
Rendha <rendhazuper@gmail.com>
2024-06-13 15:41:50 +0700

    UAS

--- Kendaraandll.java
+++ Kendaraandll.java
@@ -5,12 +5,17 @@ import java.util.*;
  */
 public class Kendaraandll {
 
-    Kendaraan head;
-    int size  = 0;
+    Kendaraan headKendaraan;
+    TransaksiPajak headTransaksiPajak;
+    int sizeKendaraan = 0;
+    int sizeTransaksiPajak = 0;
     int pilih = -1;
 
-    public boolean kosong(){
-        return head == null;
+    public boolean kosongKendaraan(){
+        return headKendaraan == null;
+    }
+    public boolean kosongTransaksi(){
+        return headTransaksiPajak == null;
     }
 
     public int menu(){
@@ -25,17 +30,17 @@ public class Kendaraandll {
         return pilih = input.nextInt();
     }
         public void tambahkendaraan(String noTNKB, String nama, String jenis, int cc, int tahun, int bulanHarusBayar){
-        if (kosong()) {
-           head = new Kendaraan(null, noTNKB, nama, jenis, cc, tahun, bulanHarusBayar, null);
+        if (kosongKendaraan()) {
+           headKendaraan = new Kendaraan(null, noTNKB, nama, jenis, cc, tahun, bulanHarusBayar, null);
         }else{ 
-            Kendaraan current = head; 
+            Kendaraan current = headKendaraan; 
             while (current.next !=null) {
                 current = current.next;
             }
             Kendaraan dataBaru = new Kendaraan(current, noTNKB, nama, jenis, cc, tahun, bulanHarusBayar, null);
             current.next = dataBaru;
         }
-        size++;
+        sizeKendaraan++;
 
     }
     public void tambahdata(){
@@ -45,13 +50,45 @@ public class Kendaraandll {
         tambahkendaraan("B 1234 AG", "Sisa", "Motor", 150, 2020, 1);
     }
 
+    public long hitung(Kendaraan kendaraan){
+        if (kendaraan.jenis.equalsIgnoreCase("Motor")) {
+            if (kendaraan.cc < 100) {
+                return 100000;
+            }else if (kendaraan.cc > 100 && kendaraan.cc < 250){
+                return 250000;
+            }else{
+                return 500000;
+            }
+        } else if (kendaraan.jenis.equalsIgnoreCase("Mobil")) {
+            if (kendaraan.cc < 1000) {
+                return 750000;
+            } else if (kendaraan.cc > 1000 && kendaraan.cc <2500) {
+                return 1000000;
+            } else {
+                return 1500000;
+            }
+        }
+        return 0;
+    }
+
+    public long denda(int bulanHarusBayar, int bulanBayar){
+        int selisihbulan = bulanBayar - bulanHarusBayar;
+        if (selisihbulan <= 0) {
+           return 0; 
+        }else if(selisihbulan <=3){
+        return 50000 ;
+        }else{
+        return 50000 * selisihbulan;
+    }
+}
+
     public void tampilkanData() {
-        if (kosong()) {
+        if (kosongKendaraan()) {
             System.out.println("Kosong ges");
         } else {
             System.out.printf("|%-15s |%-20s |%-15s |%-15s |%-10s |%-15s%n", 
                               "Nomor TNKB", "Nama Pemilik", "Jenis", "CC Kendaraan", "Tahun", "Bulan Harus Bayar");
-            Kendaraan current = head;
+            Kendaraan current = headKendaraan;
             while (current != null) {
                 System.out.printf("|%-15s |%-20s |%-15s |%-15d |%-10d |%-15d%n", 
                                   current.noTNKB, current.nama, current.jenis, current.cc, current.tahun, current.bulanHarusBayar);
